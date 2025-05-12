b2588ac21b378b951da200527a8f857513d897f5
Marcy <marcelly.nascimento84@gmail.com>
2025-05-12 20:04:29 -0300

    chore: instalando bibliotecas e adicionando typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface user {
+  name: string;
+  age: number;
+}
+
+const user: user = {
+  name: 'Marcelly linda dms slk...',
+  age: 20
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{`Name: ${user.name}, Age: ${user.age}`}</Text>
       <StatusBar style="auto" />
     </View>
   );
