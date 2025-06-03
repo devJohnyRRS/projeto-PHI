ab91219c053debe4373798db2ae0db5fcc6845dc
Marcy <marcelly.nascimento84@gmail.com>
2025-06-03 17:04:23 -0300

    chore: organizando mocks na pasta correta

--- src/screens/Artigos/index.tsx
+++ src/screens/Artigos/index.tsx
@@ -5,55 +5,7 @@ import theme from '../../assets/themes/THEMES'
 import { CaretLeft, Newspaper, SlidersHorizontal } from 'phosphor-react-native'
 import { useTypedNavigation } from '../../hooks/useNavigate'
 import PostCard from '../../components/PostCard'
-import { MediaPost } from '../../types/Posts'
-
-const mockArtigos: MediaPost[] = [
-    {
-        id: 101,
-        type: 'artigo',
-        title: 'Revolução Industrial',
-        description: 'Por Revolução Industrial, as ciências humanas compreendem como o período de grande desenvolvimento tecnológico que foi iniciado na Inglaterra a partir da segunda metade do século XVIII. Com o tempo, esse desenvolvimento espalhou-se para outras partes do mundo, como a Europa ocidental e os Estados Unidos. Assim, surgiu a indústria, e as transformações causadas por essa possibilitaram a consolidação do capitalismo. A economia, a nível mundial, sofreu grandes transformações. O processo de produção de mercadorias acelerou-se bastante, já que a produção manual foi substituída pela utilização da máquina. O resultado foi o estímulo à exploração dos recursos da natureza de maneira excessiva, uma vez que a capacidade produtiva aumentou. A Revolução Industrial também impactou as relações de trabalho, gerando uma reação dos trabalhadores, cada vez mais explorados no contexto industrial.',
-        link: 'https://mundoeducacao.uol.com.br/historiageral/revolucao-industrial-2.htm',
-        stats: {
-            badge: ['História', 'Sociedade'],
-            time: '2h atrás',
-            content: 'Entenda como o avanço industrial modificou os padrões de vida e as relações de trabalho.',
-            comments: 128,
-            likes: 2043,
-            dislikes: 12,
-        },
-    },
-    {
-        id: 102,
-        type: 'artigo',
-        title: 'Ética na Inteligência Artificial: OS DESAFIOS DO SÉCULO XXI',
-        description: 'A inteligência artificial está se tornando uma força cada vez mais poderosa em nossas vidas, influenciando desde recomendações de filmes até decisões médicas e políticas. Com esse crescimento, surge uma questão crucial: como garantir que a IA seja utilizada de maneira ética e responsável? A ética na inteligência artificial abrange uma variedade de preocupações, desde a privacidade e a segurança até a imparcialidade e a transparência. Uma das principais preocupações éticas é o viés algorítmico, que pode resultar em discriminação injusta quando os sistemas de IA são treinados com conjuntos de dados enviesados. Isso levanta questões profundas sobre justiça social e igualdade de oportunidades.',
-        link: 'https://www.dio.me/articles/etica-na-inteligencia-artificial-os-desafios-do-seculo-xxi',
-        stats: {
-            badge: ['Tecnologia', 'Ética'],
-            time: '1 dia atrás',
-            content: 'Os riscos e benefícios do uso de IA em contextos sensíveis como saúde, segurança e educação.',
-            comments: 332,
-            likes: 3201,
-            dislikes: 45,
-        },
-    },
-    {
-        id: 103,
-        type: 'artigo',
-        title: 'Mudanças climáticas',
-        description: 'As mudanças climáticas são, sem dúvidas, um dos maiores desafios da sociedade atual. Apesar de sempre usarmos o urso-polar como símbolo dessas mudanças, as alterações no clima estão longe de atingir apenas esses animais. Os impactos das mudanças climáticas são significativos e afetam desde a nossa saúde até a produção de alimentos. A seguir, você entenderá melhor o que são as mudanças climáticas e como elas afetam a nossa vida e a dos outros seres vivos do planeta.',
-        link: 'https://brasilescola.uol.com.br/biologia/mudancas-climaticas.htm',
-        stats: {
-            badge: ['Meio ambiente', 'Atualidades'],
-            time: '3 dias atrás',
-            content: 'Conheça os principais fatores que contribuem para a mudança do clima e como combatê-los.',
-            comments: 274,
-            likes: 4102,
-            dislikes: 22,
-        },
-    },
-];
+import { mockPostsArtigos } from '../../Mocks/mockPostsArtigos'
 
 export default function Artigos() {
 
@@ -91,7 +43,7 @@ export default function Artigos() {
 
                 </View>
 
-                {mockArtigos.map((artigo) => (
+                {mockPostsArtigos.map((artigo) => (
                     <PostCard key={artigo.id} post={artigo}>
                         <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 4 }}>{artigo.title}</Text>
 
--- src/screens/Videos/index.tsx
+++ src/screens/Videos/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import PostCard from '../../components/PostCard'
-import { MediaPost } from '../../types/Posts';
 import PerfilHeader from '../../components/PerfilHeader';
 import YouTubeEmbed from '../../components/YotubeEmbed';
 import { CaretLeft, SlidersHorizontal, Video } from 'phosphor-react-native';
 import theme from '../../assets/themes/THEMES';
 import { useTypedNavigation } from '../../hooks/useNavigate';
+import { mockPostsVideos } from '../../Mocks/mockPostsVideo';
 
 export default function Videos() {
 
@@ -15,49 +15,6 @@ export default function Videos() {
     const handleGoBack = () => {
         navigation.goBack();
     }
-
-    const mockPosts: MediaPost[] = [
-        {
-            id: 1,
-            type: 'video',
-            title: 'O que foi o Holocausto?',
-            link: 'ylcJ4j5Sx-I',
-            stats: {
-                badge: ['História'],
-                time: '30min atrás',
-                content: 'Aprenda sobre bancos de dados relacionais e não relacionais.',
-                comments: 209,
-                likes: 400,
-                dislikes: 0,
-            },
-        },
-        {
-            id: 2,
-            type: 'video',
-            title: 'A RESPOSTA PARA QUALQUER DANO MÁGICO NA TOP LANE',
-            link: 'W6hnaV90-k4',
-            stats: {
-                badge: ['League of Legends', 'Games'],
-                time: '30min atrás',
-                comments: 120,
-                likes: 694,
-                dislikes: 89,
-            },
-        },
-        {
-            id: 3,
-            type: 'video',
-            title: 'MODELOS ATÔMICOS: Dalton, Thomson, Rutherford e Rutherford-Bohr',
-            link: '5-fa4IKp5bU',
-            stats: {
-                badge: ['Química', 'Ciências'],
-                time: '30min atrás',
-                comments: 122,
-                likes: 4565,
-                dislikes: 19,
-            },
-        },
-    ];
 
     return (
         <ScrollView style={{ flex: 1, backgroundColor: theme.colors.background }}>
@@ -86,7 +43,7 @@ export default function Videos() {
 
                 </View>
 
-                {mockPosts.map((post) => (
+                {mockPostsVideos.map((post) => (
                     <PostCard key={post.id} post={post}>
                         {/* <Text style={{ fontSize: 16 }}>{post.stats.content}</Text> */}
                         <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{post.title}</Text>
